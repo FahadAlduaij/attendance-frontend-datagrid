fd20653f7f851fe01b5bc8949f7224e706420416
FahadAlduaij <fahad_osama@hotmail.com>
2021-12-27 13:58:01 +0300

    to win

--- attendance/src/components/Home/TableData.js
+++ attendance/src/components/Home/TableData.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { observer } from "mobx-react";
+import moment from "moment";
 
 // stores
 import authStore from "../../stores/authStore";
 import profileStore from "../../stores/profileStore";
 
 function TableData({ absent }) {
-	const [state, setstate] = React.useState("");
+	const [state, setState] = React.useState("");
 	const columns = [
 		{ field: "id", headerName: "ID", width: 170 },
 		{ field: "user", headerName: "Name", width: 200 },
@@ -37,30 +38,17 @@ function TableData({ absent }) {
 			headerName: "To",
 			width: 200,
 		},
-		// {
-		// 	field: "date",
-		// 	headerName: "emergency leave",
-		// 	description: "This column has a value getter and is not sortable.",
-		// 	sortable: true,
-		// 	width: 200,
-		// 	valueGetter: (params) =>
-		// 		`${params.getValue(params.id, "firstName") || ""} ${
-		// 			params.getValue(params.id, "lastName") || ""
-		// 		}`,
-		// },
 	];
 
 	const rows = absent.map((item) => ({
 		id: item._id,
 		user: item.user.name,
-		day: item.day,
-		date: item.date,
+		day: moment(item.day).format("dddd"),
+		date: moment(item.date).format("DD/MM/yyyy"),
 		type: item.type,
-		from: item.from,
-		to: item.to,
+		from: moment(item.from).format("LT"),
+		to: moment(item.to).format("LT"),
 	}));
-
-	console.log(state);
 
 	return (
 		<div style={{ width: "95%" }}>
@@ -72,7 +60,7 @@ function TableData({ absent }) {
 				pageSize={100}
 				rowsPerPageOptions={[100]}
 				checkboxSelection
-				onSelectionModelChange={(e) => setstate(e)}
+				onSelectionModelChange={(e) => setState(e)}
 			/>
 		</div>
 	);
